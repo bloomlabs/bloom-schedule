e6bcc7a1b5da8ec3f866e520b957344073bdd01d
Damian Worsdell <damian@djw.net.au>
2015-10-06 16:37:10 +0800

    Adding other component skeletons

---
+++ dashboard/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Bloom Schedule
+ * Live Dashboard
+ *
+ * Distribution, modification or use of this source code without
+ * authors permission is strictly forbidden under any circumstance.
+ * By using this software you agree to our terms of use and service.
+ *
+ * @abstract    Bloom Schedule - Dashboard
+ * @author      Damian Worsdell <damian@djw.net.au>
+ * @copyright   Copyright (C) 2015, Damian Worsdell and Bloom Labs, Inc.
+ */
+
